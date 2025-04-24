00d8c168524622e9c8ada602d2825fd2a808f8cb
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-04-24 13:07:26 +0200

    feat: enhance ExtendedVelocity plugin with initialization and shutdown events

--- src/main/kotlin/dev/thebjoredcraft/extendedvelocity/ExtendedVelocity.kt
+++ src/main/kotlin/dev/thebjoredcraft/extendedvelocity/ExtendedVelocity.kt
@@ -1,17 +1,54 @@
 package dev.thebjoredcraft.extendedvelocity
 
+import com.google.gson.Gson
 import com.google.inject.Inject
+import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
+import com.velocitypowered.api.event.proxy.ProxyShutdownEvent
 import com.velocitypowered.api.plugin.Plugin
+import com.velocitypowered.api.plugin.PluginContainer
+import com.velocitypowered.api.plugin.annotation.DataDirectory
 import com.velocitypowered.api.proxy.ProxyServer
+import org.slf4j.Logger
+import java.nio.file.Path
 
-@Plugin(
+@Plugin (
     id = "extendedvelocity",
     name = "ExtendedVelocity",
     version = "1.0.0-SNAPSHOT",
     description = "Do more with your proxy.",
-    authors = ["TheBjoRedCraft"]
+    authors = ["TheBjoRedCraft"],
+    url = "https://github.com/TheBjoRedCraft/ExtendedVelocity/",
+    dependencies = []
 )
 class ExtendedVelocity {
     @Inject
+    lateinit var logger: Logger
+
+    @Inject
     lateinit var proxy: ProxyServer
+
+    @Inject
+    lateinit var pluginContainer: PluginContainer
+
+    @Inject
+    @DataDirectory
+    lateinit var dataFolder: Path
+
+    @Subscribe
+    fun onInitialization(event: ProxyInitializeEvent) {
+
+    }
+
+    @Subscribe
+    fun onShutdown(event: ProxyShutdownEvent) {
+
+    }
+
+    companion object {
+        val INSTANCE = ExtendedVelocity()
+    }
 }
+
+val gson = Gson()
+val plugin get() = ExtendedVelocity.INSTANCE
