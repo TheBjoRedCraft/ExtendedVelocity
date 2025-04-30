4c8c3b0d304b3114e928c3e91f26e5a6d7ff73b6
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-04-30 20:42:20 +0200

    feat: enhance ListCommand to display online players and server-specific information

--- src/main/kotlin/dev/thebjoredcraft/extendedvelocity/command/ListCommand.kt
+++ src/main/kotlin/dev/thebjoredcraft/extendedvelocity/command/ListCommand.kt
@@ -5,8 +5,11 @@ import com.velocitypowered.api.command.SimpleCommand
 
 import dev.thebjoredcraft.extendedvelocity.message.MessageBuilder
 import dev.thebjoredcraft.extendedvelocity.plugin
+import dev.thebjoredcraft.extendedvelocity.util.error
 import dev.thebjoredcraft.extendedvelocity.util.sendRawText
 import dev.thebjoredcraft.extendedvelocity.util.sendText
+import net.kyori.adventure.text.Component
+import kotlin.jvm.optionals.getOrNull
 
 class ListCommand: SimpleCommand {
     override fun execute(invocation: SimpleCommand.Invocation) {
@@ -18,11 +21,58 @@ class ListCommand: SimpleCommand {
             return
         }
 
-        source.sendText(MessageBuilder().modernGreen("The proxy server is running ${plugin.proxy.version.name} ${plugin.proxy.version.version} by ${plugin.proxy.version.vendor} on ${System.getProperty("os.name")} ${System.getProperty("os.version")}"))
+
+        if(args.isEmpty()) {
+            val players = plugin.proxy.allPlayers.sortedByDescending { it.currentServer.getOrNull()?.serverInfo?.name }
+
+            if (players.isEmpty()) {
+                source.error("There are no players online.")
+                return
+            }
+
+            val playerList = players.joinToString(", ") { it.username }
+
+            source.sendText(MessageBuilder()
+                .modernGreen("There are ${players.size} players online: ")
+                .white(playerList)
+            )
+
+        } else {
+            val serverName = args[0]
+
+            val server = plugin.proxy.getServer(serverName).getOrNull() ?: run {
+                source.sendText(MessageBuilder().error("Server $serverName not found."))
+                return
+            }
+
+            val players = server.playersConnected.sortedByDescending { it.username }
+
+            if (players.isEmpty()) {
+                source.error("There are no players online on $serverName.")
+                return
+            }
+
+            val playerList = players.joinToString(", ") { it.username }
+
+            source.sendText(MessageBuilder()
+                .modernGreen("There are ${players.size} players online on $serverName: ")
+                .white(playerList)
+            )
+        }
     }
 
     override fun hasPermission(invocation: SimpleCommand.Invocation): Boolean {
         return invocation.source().hasPermission("extendedvelocity.command.list")
+    }
+
+    override fun suggest(invocation: SimpleCommand.Invocation): List<String> {
+        val args = invocation.arguments()
+
+        if (args.size <= 1) {
+            return plugin.proxy.allServers.map { it.serverInfo.name }
+        }
+
+        return emptyList()
     }
 
     private fun sendUsage(source: CommandSource) {
